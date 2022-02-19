512410461154024b4ed5fd3482d6223b56dc8b76
Joseph Adams <josephdadams@gmail.com>
2022-02-19 15:36:05 -0500

    initial commit

---
+++ src/actions.js
@@ -0,0 +1,56 @@
+module.exports = {
+	// ##########################
+	// #### Instance Actions ####
+	// ##########################
+
+	setActions: function (i) {
+		let self = i;
+		let actions = {};
+
+		actions.recordingControl = {
+			label: 'Recording Control',
+			options: [
+				{
+					type: 'dropdown',
+					label: 'Command',
+					id: 'command',
+					default: 'start',
+					choices: [
+						{ id: 'start', label: 'Start' },
+						{ id: 'stop', label: 'Stop' }
+					]
+				}
+			],
+			callback: function (action, bank) {
+				self.sendCommand(self.recPrefix + '/' + action.options.command, {});
+			}
+		};
+
+		actions.streamingControl = {
+			label: 'Streaming Control',
+			options: [
+				{
+					type: 'dropdown',
+					label: 'Command',
+					id: 'command',
+					default: 'publish',
+					choices: [
+						{ id: 'publish', label: 'Publish' },
+						{ id: 'unpublish', label: 'Unpublish' },
+						{ id: 'broadcast', label: 'Broadcast' },
+						{ id: 'halt', label: 'Halt' },
+						{ id: 'cancel', label: 'Cancel' },
+						{ id: 'preview', label: 'Preview' },
+						{ id: 'endpreview', label: 'End Preview' },
+						{ id: 'complete', label: 'Complete' }
+					]
+				}
+			],
+			callback: function (action, bank) {
+				self.sendCommand(self.streamPrefix + '/' + action.options.command, {});
+			}
+		};
+
+		return actions;
+	}
+}
